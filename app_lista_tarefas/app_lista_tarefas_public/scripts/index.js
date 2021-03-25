f98e11efdae8831f0e5585ebc9abec82bf03ed17
Caio-4lberto <73459155+Caio-4lberto@users.noreply.github.com>
2021-03-25 10:44:06 -0300

    Projeto Completo

---
+++ app_lista_tarefas/app_lista_tarefas_public/scripts/index.js
@@ -0,0 +1,62 @@
+		
+		function editar(id, txt_tarefa){
+			
+			//CRIAR UM FORM DE EDIÇÃO
+			let form = document.createElement('form');
+			form.action = 'index.php?pag=index&acao=atualizar';
+			form.method = 'post';
+			form.className = 'row'; 
+
+			//CRIAR UM IMPUT PARA ENTRADA DO TEXTO
+			let inputTarefa = document.createElement('input');
+			inputTarefa.type = 'text';
+			inputTarefa.name = 'tarefa';
+			inputTarefa.className = 'col-9 form-control';
+			inputTarefa.value = txt_tarefa
+
+			//CRIAR UM INPUT HIDDEN PARA GUARADAR O ID DA TAREFA
+			let inputId = document.createElement('input');
+			inputId.type = 'hidden';
+			inputId.name = 'id';
+			inputId.value = id
+
+			//CRIAR UM BOTÃO PARA ENVIO DO FORM
+			let button = document.createElement('button');
+			button.type = 'submit';
+			button.className = 'col-3 btn btn-info';
+			button.innerHTML = 'Atualizar';
+
+			//INCLUIR INPUTTAREFA NO FORM
+			form.appendChild(inputTarefa);
+
+			//INCLUIR INPUTID NO FORM
+			form.appendChild(inputId);
+
+			//INCLUIR BUTTON NO FORM
+			form.appendChild(button);
+
+			//TESTE
+			//console.log(form);
+
+			//alert(id);
+
+			//SELECIONAR A DIV TAREFA
+			let tarefa = document.getElementById('tarefa_'+id);
+
+			//LIMPAR O TEXTO DA TAREFA PARA INCLUSÃO DO FORM
+			tarefa.innerHTML = '';
+
+			//INCLUIR FORM NA PÁGINA
+			tarefa.insertBefore(form, tarefa[0]);
+
+
+		}
+		
+		function remover(id) {
+			location.href = 'index.php?pag=index&acao=remover&id='+id;
+		}
+
+		function marcarRealizada(id) {
+			location.href = 'index.php?pag=index&acao=marcarRealizada&id='+id;
+		}
+	
